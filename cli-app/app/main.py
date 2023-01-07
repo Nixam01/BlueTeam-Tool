d2e2d17cbc995da55ce2170af01c43638b4673da
jignasia <01159008@pw.edu.pl>
2023-01-07 15:52:40 +0100

    application def agent

--- cli-app/app/main.py
+++ cli-app/app/main.py
@@ -124,5 +124,91 @@ def read_file(file_path, re_pattern, grep_pattern, bpf_filter):
     #log_file.write("\n" + " read_file: " + str(output) + "\n\n")
     #log_file.close()
 
+
+@application.command()
+@click.option('--file_path', multiple=True, type=click.Path(exists=True))
+@click.option('--rules', multiple=True)
+@click.option('--firewall', multiple=False, required=False, default="", help="ip:port")
+@click.option('--console', multiple=False, required=False, default="", help="ip:port")
+
+
+@application.command()
+@click.option('--action', multiple=False, help="Action you want to perform (one of netconfig)")
+@click.option('--agent_host', multiple=False, help="ip:port")
+@click.option('--interface', multiple=False, help="Interface to capture traffic on")
+@click.option('--capture_filter', default="", multiple=False, help="Capture filter")
+@click.option('--timeout', multiple=False, help="Time of capturing")
+@click.option('--file_number', multiple=True, help="Number of file to download")
+@click.option('--command', multiple=False, help="Command to execute")
+
+def agent(action, agent_host, interface, capture_filter, timeout, file_number, command):
+    if action == 'netconfig':
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        r = requests.get(f'http://{agent_host}/netconfig', headers=headers)
+        result = str(r.content).replace('\\n', '\n').replace('\\t', '\t')
+        click.echo(result)
+
+    elif action == 'capture':
+        pload = {"interface": interface, "filter": capture_filter, "timeout": str(timeout)}
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        response = requests.post(f'http://{agent_host}/capture', data=json.dumps(pload), headers=headers, stream=True)
+        file_name = "Files_application/pcaps/" + str(datetime.now().strftime("%d-%m-%Y_%H-%M-%S")) + ".pcap"
+        if response.status_code == 200:
+            with open(file_name, 'wb') as f:
+                f.write(response.content)
+
+    elif action == 'list_pcaps':
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        r = requests.get(f'http://{agent_host}/list-pcaps', headers=headers)
+        click.echo(r.content)
+
+    elif action == 'list_logs':
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        r = requests.get(f'http://{agent_host}/list-logs', headers=headers)
+        click.echo(r.content)
+
+    elif action == 'download_pcap':
+        for file in file_number:
+            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+            r = requests.get(f'http://{agent_host}/list-pcaps', headers=headers)
+            json_str = str(r.content)
+            json_str = json_str[3:-2]
+            list = json_str.split(',')
+            file_name = "Files_application/pcaps/" + list[int(file)-1][4:-1]
+
+            parameters = {"nr": file}
+            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+            response = requests.get(f'http://{agent_host}/download-pcap', params=parameters, headers=headers, stream=True)
+            if response.status_code == 200:
+                with open(file_name, 'wb') as f:
+                    f.write(response.content)
+
+    elif action == 'download_log':
+        for file in file_number:
+            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+            r = requests.get(f'http://{agent_host}/list-logs', headers=headers)
+            json_str = str(r.content)
+            json_str = json_str[3:-2]
+            list = json_str.split(',')
+            file_name = "Files_application/logs/" + list[int(file)-1][4:-1]
+
+            parameters = {"nr": file}
+            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+            response = requests.get(f'http://{agent_host}/download-log', params=parameters, headers=headers, stream=True)
+            if response.status_code == 200:
+                with open(file_name, 'wb') as f:
+                    f.write(response.content)
+    elif action == 'command':
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        payload = {"command": command}
+        r = requests.post(f'http://{agent_host}/command', headers=headers, data=json.dumps(payload))
+        result = str(r.content).replace('\\n', '\n').replace('\\t', '\t')
+        click.echo(result)
+
+    else:
+        click.echo("Invalid action")
+
+
 if __name__ == '__main__':
+    application()
     print('CLI Application Started...')
